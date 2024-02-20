80c0b845aa917d6c923aee341b512ba49923f9be
tonyshu <tonynshu@gmail.com>
2024-02-20 09:39:13 +0800

    finished

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+
+    public Song (String a, String b){
+        title = a;
+        artist = b;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+    private ArrayList<String> listens = new ArrayList<>();
+    public int howMany(ArrayList<String> arrayList) {
+        int original = listens.size();
+        for (String s: arrayList) {
+            if (!listens.contains(s.toLowerCase())) listens.add(s.toLowerCase());
+        }
+        return listens.size()-original;
+    }
+
+
 }
